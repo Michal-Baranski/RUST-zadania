c7da0df999c65e0875a2fc0910dbdc37ea0e181b
mbarabski <131877633+mbarabski@users.noreply.github.com>
2023-05-03 18:23:10 +0200

    Create 202-Happy-Number.rs

---
+++ 202-Happy-Number.rs
@@ -0,0 +1,41 @@
+impl Solution {
+    pub fn is_happy(n: i32) -> bool {
+        let mut set = std::collections::HashSet::new();
+
+        let mut num = n;
+
+        while num !=1 
+        {
+            let mut sum = 0;
+
+            while num > 0
+            {
+                let digit = num %10;
+                sum += digit * digit;
+                num /= 10;
+            }
+
+            if set.contains(&sum)
+            {
+                return false;
+            }
+
+            set.insert(sum);
+            num = sum;
+        }
+
+        true
+        
+    }
+}
+
+/*
+Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+*/
